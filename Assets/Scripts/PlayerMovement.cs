b38082b50001b0588ff5d54a6f0488ce2092aac6
TaliaKah <anaissaratagougne@tutanota.com>
2024-06-08 12:06:33 +0200

    Add character +,atlas

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    float horizontal;
+    float vertical;
+    public float runSpeed = 20.0f;
+
+
+    void Start()
+    {
+        rb= GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2 (horizontal * runSpeed, vertical * runSpeed);
+    }
+
+}
